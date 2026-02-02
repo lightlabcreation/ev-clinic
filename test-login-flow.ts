ad26902bf946ac3b0ca8edc02cc9abb9e7b8241a
GautamBairagi <gauatambairagi22@gmail.com>
2026-02-02 17:36:10 +0530

    dfdsdfssdfd

--- test-login-flow.ts
+++ test-login-flow.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://ev-clinic.kiaantechnology.com/app/api';
 
 async function testLogin(email: string, password: string) {
     try {
